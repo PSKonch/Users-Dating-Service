c640a9470da7b2f590b3769235c0bc8752367bb3
Dimitriy Shaban <shaban.benito.dimitri@gmail.com>
2024-10-29 12:17:04 +0300

    celery-redis-conn

--- src/config.py
+++ src/config.py
@@ -7,9 +7,16 @@ class Settings(BaseSettings):
     DB_PORT: int
     DB_NAME: str
 
+    REDIS_HOST: str
+    REDIS_PORT: int
+
     @property
     def DB_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+    @property
+    def REDIS_URL(self):
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}"
     
     model_config = SettingsConfigDict(env_file=".env")
 
--- src/main.py
+++ src/main.py
@@ -1,3 +1,15 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+
+from src.init import redis_manager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await redis_manager.connect()
+    FastAPICache.init(redis_manager.redis, prefix=['fastapi_cache'])
+    yield 
+    await redis_manager.close()
+
+app = FastAPI(lifespan=lifespan)
