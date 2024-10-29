8b9bf8a22b3599d2533ffd71c0756be6e63b249f
Dimitriy Shaban <shaban.benito.dimitri@gmail.com>
2024-10-29 11:07:47 +0300

    db_conn

---
+++ src/migrations/versions/cb427432b3d2_init_migration.py
@@ -0,0 +1,27 @@
+"""init migration
+
+Revision ID: cb427432b3d2
+Revises: 
+Create Date: 2024-10-29 11:06:02.788495
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "cb427432b3d2"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
